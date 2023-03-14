5c6746013e91746211817c7256e94308bb223cc8
zakaria-akash <zakaria.93@yahoo.com>
2023-03-14 08:56:35 +0600

    adding separate http-error component as a model to include all error logic and adding separate place-controllers component to include all place related logic as a controller as part of the MVC structure

--- app.js
+++ app.js
@@ -7,4 +7,15 @@ const app = express();
 
 app.use("/api/places", placesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({
+    message: error.message || "An unkonown error occured!",
+  });
+});
+
 app.listen(5000);
---
+++ controllers/places-controllers.js
@@ -0,0 +1,78 @@
+const HttpError = require("../models/http-error");
+
+const Sample_Places = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description:
+      "One of the most famous sky scrapers in the world! The Empire State Building is a 102-story Art Deco skyscraper in Midtown Manhattan, New York City.",
+    imageUrl: "https://media.timeout.com/images/101705309/image.jpg",
+    address: "20 W 34th St., New York, NY 10001, United States",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Madaripur Sokuni Lake",
+    description:
+      "Sokuni Lake is a spectacular place in the natural beauty of Madaripur district. It is located at the heart of Madaripur city",
+    imageUrl:
+      "https://vromonguide.com/wp-content/uploads/madaripur-sokuni-lake.jpg",
+    address: "Madaripur Sadar, Madaripur, Dhaka, Bangladesh 7900",
+    location: {
+      lat: 23.1661525,
+      lng: 90.2058498,
+    },
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    title: "Madaripur Sokuni Lake",
+    description:
+      "Sokuni Lake is a spectacular place in the natural beauty of Madaripur district. It is located at the heart of Madaripur city",
+    imageUrl:
+      "https://vromonguide.com/wp-content/uploads/madaripur-sokuni-lake.jpg",
+    address: "Madaripur Sadar, Madaripur, Dhaka, Bangladesh 7900",
+    location: {
+      lat: 23.1661525,
+      lng: 90.2058498,
+    },
+    creator: "u3",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  console.log("Get request in places-router!");
+
+  const placeId = req.params.pid;
+
+  const place = Sample_Places.find((place) => place.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for given id.");
+  }
+
+  res.json({ place });
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = Sample_Places.find((place) => place.creator === userId);
+
+  if (!place) {
+    const error = new HttpError(
+      "Could not find a place for given user id.",
+      404
+    );
+    return next(error);
+  }
+
+  res.json({ place });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
