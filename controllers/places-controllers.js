65a4d14a4bb24caa6d7b9cd38f473b982f8b8720
zakaria-akash <zakaria.93@yahoo.com>
2023-03-15 11:04:18 +0600

    adding new post route to create new sample places and test post route through Postman post request

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 
 const Sample_Places = [
@@ -74,5 +75,23 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  };
+
+  Sample_Places.push(createdPlace);
+
+  res.status(201).json({ place: createdPlace });
+};
+
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
+exports.createPlace = createPlace;
